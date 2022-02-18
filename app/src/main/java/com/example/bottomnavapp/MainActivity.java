a19d0f41fc903108d0f72aae2b539b919fa9e289
harsh gandhi <harshgandhi043@gmail.com>
2022-02-18 18:13:57 +0530

    made bottom navigation bar using bottom navbar material ui and fragments

---
+++ app/src/main/java/com/example/bottomnavapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.bottomnavapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragmentContainerView,new HomeFragment())
+                .commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(navItemSelectedListener);
+    }
+
+    // NAVBAR ITEM SELECTED LISTENER
+    private NavigationBarView.OnItemSelectedListener navItemSelectedListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.homeMenuItem:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profileMenuItem:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.settingsMenuItem:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+            return true;
+        };
+    };
+
+    // REPLACE FRAGMENT METHOD
+    private void replaceFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView,fragment)
+                .commit();
+    }
+}
